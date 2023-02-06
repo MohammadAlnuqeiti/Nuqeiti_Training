7de3166981d52fe384f27dff5f02311698dae99e
malnuqeiti-988 <mohammadalnuqeiti@gmail.com>
2023-02-06 19:46:34 +0300

    edit status

--- app/Http/Controllers/User/LoginUserController.php
+++ app/Http/Controllers/User/LoginUserController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
 use App\Models\LoginUser;
 
 use Illuminate\Http\Request;
@@ -20,21 +22,33 @@ class LoginUserController extends Controller
     public function LoginPost(LoginRequest $request): RedirectResponse
     {
 
-        // $request->authenticate();
-        $request->authenticate();
+        $data = User::where('email',$request->email)->get();
+        // dd($data[0]->role );
+        if($data[0]->role=='engineer'){
 
-        $request->session()->regenerate();
-
-         $email=$request->email;
-        $password=$request->password;
-
-        if(Auth::attempt(['email'=>$email,'password'=>$password]))
-        {
-            // $request->session()->regenerate();
-
-            return redirect()->route('user.index');
-
+            if($data[0]->status !=='accept'){
+                abort(403);
+            }
         }
+            // $request->authenticate();
+            $request->authenticate();
+
+            $request->session()->regenerate();
+
+                $email=$request->email;
+            $password=$request->password;
+
+            if(Auth::attempt(['email'=>$email,'password'=>$password]))
+            {
+                // $request->session()->regenerate();
+
+                return redirect()->route('user.index');
+
+            }
+        
+
+
+
     }
     public function destroy(Request $request): RedirectResponse
     {
--- resources/views/publicUser/layouts/main-header.blade.php
+++ resources/views/publicUser/layouts/main-header.blade.php
@@ -37,43 +37,33 @@
                 <a href="{{route('user.login.destroy')}}" class="nav-link">logout</a>
             </li>
             @endif
+            @if(!Auth::user())
+            <li class="nav-item">
+                <!-- <div class="dropdown">
+                    <button class="dropbtn">REGISTER
+                        <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="./register.html">REGISTER AS STUDENT</a>
+                        <a href="./registerEngineering.html">REGISTER AS ENGINEERING</a>
+                    </div>
+                </div> -->
+                <a href="{{route('user.register')}}"><button id="btn-nav" class="nav-link">REGISTER</button></a>
+            </li>
+            @endif
+            @if(Auth::user())
+            <li class="nav-item">
+                @if(Auth::user()->role=="user" || Auth::user()->role=="admin")
+                <a href="{{route('user.profile_user.index')}}"><button id="btn-nav" class="nav-link">ACCOUNT</button></a>
+                @endif
+                @if(Auth::user()->role=="engineer")
+                <a href="{{route('user.profile_engineer.index')}}"><button id="btn-nav" class="nav-link">ACCOUNT</button></a>
+                @endif
+            </li>
+            @endif
+            {{-- <a href="{{route('user.profile.index')}}" class="btn btn-primary rounded-pill py-2 px-4">Account</a> --}}
+            </ul>
         </div>
-        @if(!Auth::user())
-        <li class="nav-item">
-            <!-- <div class="dropdown">
-                <button class="dropbtn">REGISTER
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="./register.html">REGISTER AS STUDENT</a>
-                    <a href="./registerEngineering.html">REGISTER AS ENGINEERING</a>
-                </div>
-            </div> -->
-            <a href="{{route('user.register')}}"><button id="btn-nav" class="nav-link">REGISTER</button></a>
-        </li>
-        @endif
-        @if(Auth::user())
-        <li class="nav-item">
-            <!-- <div class="dropdown">
-                <button class="dropbtn">REGISTER
-                    <i class="fa fa-caret-down"></i>
-                </button>
-                <div class="dropdown-content">
-                    <a href="./register.html">REGISTER AS STUDENT</a>
-                    <a href="./registerEngineering.html">REGISTER AS ENGINEERING</a>
-                </div>
-            </div> -->
-            @if(Auth::user()->role=="user" || Auth::user()->role=="admin")
-            <a href="{{route('user.profile_user.index')}}"><button id="btn-nav" class="nav-link">ACCOUNT</button></a>
-            @endif
-            @if(Auth::user()->role=="engineer")
-            <a href="{{route('user.profile_engineer.index')}}"><button id="btn-nav" class="nav-link">ACCOUNT</button></a>
-            @endif
-
-        </li>
-        @endif
-        {{-- <a href="{{route('user.profile.index')}}" class="btn btn-primary rounded-pill py-2 px-4">Account</a> --}}
-        </ul>
          <div class="hamburger">
         <span class="bar"></span>
         <span class="bar"></span>
