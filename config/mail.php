893f400364b6265ff21316aa1cadb28c6e62707f
malnuqeiti-988 <mohammadalnuqeiti@gmail.com>
2023-03-20 10:14:29 +0300

    test

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'mailgun'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'nuqeiti@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Nuqeiti training'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
