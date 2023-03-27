2a7d0e5fc2e7d6e14fffe2ca682ce1fac4a0bb78
malnuqeiti-988 <mohammadalnuqeiti@gmail.com>
2023-03-27 16:57:13 +0300

    edit mail page

--- app/Http/Controllers/Admin/EngineeringController.php
+++ app/Http/Controllers/Admin/EngineeringController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Mail\Subscribe;
+use App\Models\Subscriber;
 use App\Models\User;
 
 use Illuminate\Http\Request;
